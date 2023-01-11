b3d636d9b7fe749188b9a01f7c49874c5f47fe30
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-11 02:43:47 +0000

    Basic oauth2 authorization server setup.

--- src/main/java/com/jamesvrooney/config/SecurityConfig.java
+++ src/main/java/com/jamesvrooney/config/SecurityConfig.java
@@ -2,46 +2,105 @@ package com.jamesvrooney.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    @Order(1)
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+
+        httpSecurity.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
+                .oidc(withDefaults());
+
+        httpSecurity.exceptionHandling( ex -> {
+            ex.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"));
+        });
+
         return httpSecurity
-                .httpBasic()
-                .and()
                 .build();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        final var userDetailsService = new InMemoryUserDetailsManager();
-//
-//        final UserDetails james = User.withUsername("james")
-//                .password(passwordEncoder().encode("password"))
-//                .authorities("read")
-//                .build();
-//
-//        final UserDetails bob = User.withUsername("bob")
-//                .password(passwordEncoder().encode("password"))
-//                .authorities("read")
-//                .build();
-//
-//        userDetailsService.createUser(james);
-//        userDetailsService.createUser(bob);
-//
-//        return userDetailsService;
-//    }
+    @Bean
+    @Order(2)
+    public SecurityFilterChain appSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin()
+                .and()
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        final var userDetailsService = new InMemoryUserDetailsManager();
+
+        final UserDetails james = User.withUsername("james")
+                .password(passwordEncoder().encode("password"))
+                .authorities("read")
+                .build();
+
+        final UserDetails bob = User.withUsername("bob")
+                .password(passwordEncoder().encode("password"))
+                .authorities("read")
+                .build();
+
+        userDetailsService.createUser(james);
+        userDetailsService.createUser(bob);
+
+        return userDetailsService;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("client")
+                .clientSecret("secret")
+                .scope(OidcScopes.OPENID)
+                .redirectUri("https://springone.io/authorized")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+
+        return  new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    @Bean
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder().build();
     }
 }
