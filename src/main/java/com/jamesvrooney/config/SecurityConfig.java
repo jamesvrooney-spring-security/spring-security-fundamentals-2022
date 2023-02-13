40a150e81568bb12f8eec85fd07834a6d6a687b5
jamesvrooney <jamesvrooney@hotmail.com>
2023-02-13 21:04:54 +0000

    Refactor & tidy up.

--- src/main/java/com/jamesvrooney/config/SecurityConfig.java
+++ src/main/java/com/jamesvrooney/config/SecurityConfig.java
@@ -2,17 +2,13 @@ package com.jamesvrooney.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
 
 //    @Bean
@@ -20,7 +16,7 @@ public class SecurityConfig {
 //        return httpSecurity
 //                .httpBasic()
 //                .and()
-//                .build();
+//                .build();Ø
 //    }
 
     @Bean
